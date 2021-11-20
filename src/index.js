408c57882fe307dcbac3d9e49eeeac159a74e224
willy <willy821209@gmail.com>
2021-11-20 15:20:47 +0800

    管理輸出-start

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import _ from 'lodash';
 import './style.css';
-import img from './img.jpg';
+import printMe from './print.js';
+
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
-  // lodash 在当前 script 中使用 import 引入
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
-  // 将图像添加到我们已经存在的 div 中。
-  const myIcon = new Image();
-  myIcon.src = img;
- 
-  element.appendChild(myIcon);
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
  
 
   return element;
